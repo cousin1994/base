c90c47299a9fe08416d278fc4d45bf1cd90f9479
cousin1994 <123456zaq>
2016-02-29 02:08:30 +0800

    新增Servlet工具类

---
+++ util/src/main/java/com/cousin/util/struts2/ServletUtils.java
@@ -0,0 +1,9 @@
+package com.cousin.util.struts2;
+/**
+ *@author 戴嘉诚 E-mail:a773807943@gmail.com
+ *@version V0.1 创建时间：2016年2月29日 上午1:49:23
+ *Http与Servlet的工具类
+ */
+public class ServletUtils {
+
+}
